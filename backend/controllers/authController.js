1cda58f65983072660609f19064362fd182bcd1e
samhithbasa <samhithbasa12@gmail.com>
2026-05-13 11:34:08 +0530

    Last commit 1

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -117,8 +117,8 @@ const forgotPassword = async (req, res) => {
 
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // use STARTTLS
+      port: 465,
+      secure: true, // use SSL
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
